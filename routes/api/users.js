6ef229fda1118263be00ee446213b55f703467c8
Rob <robdawg@gmail.com>
2018-07-29 03:13:04 -0400

    Begin profile data modeld

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -56,15 +57,23 @@ router.post('/register', (req, res) => {
               .then(user => res.json(user))
               .catch(err => console.log(err))
           });
-        })
+        });
       }
-    })
+    });
 });
 
 // @route   GET api/users/login
 // @desc    Login user / Returning JWT
 // @access  Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -73,7 +82,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       // Check for sure
       if(!user) {
-        return res.status(404).json({email: 'User not found'});
+        errors.email = "User not found"
+        return res.status(404).json(errors);
       }
 
       // Check password
@@ -96,7 +106,8 @@ router.post('/login', (req, res) => {
               );
             });
           } else {
-            return res.status(400).json({password: 'Password incorrect'});
+            errors.password = 'Password incorrect'
+            return res.status(400).json(errors);
           }
         })
 
