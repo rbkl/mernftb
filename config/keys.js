c26195fd9ef567d5d23e68bb03154aaca31349cf
Rob <robdawg@gmail.com>
2018-08-11 03:14:23 -0400

    Complete!

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
   mongoURI: 'mongodb://rob:robrob1@ds019078.mlab.com:19078/mernftb',
-  secretOrKey: 'secret'
+  secretOrKey: 'secret',
+  githubClientId: 'eb2f5963831e743a57cb',
+  githubSecret: 'd409c6161312d95e2db9518fe4e02ef5ea025873'
 }
