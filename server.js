8a6847c1cef69c8ce51b006715c2b62deb86bc12
Rob <robdawg@gmail.com>
2018-08-11 04:10:05 -0400

    Complete v2!

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ app.use('/api/posts', posts);
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-  app.get('*', (req, res)) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  }
+  });
 }
 
 const port = process.env.PORT || 5000;
