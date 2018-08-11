e19cf82bd4fb705390c5680a1791b8d788d0a016
Rob <robdawg@gmail.com>
2018-08-11 04:06:38 -0400

    Complete v2!

--- config/keys.js
+++ config/keys.js
@@ -1,1 +1,6 @@
-module.exports require('./keys_prod');
+// module.exports require('./keys_prod');
+
+module.exports = {
+  mongoURI: 'mongodb://rob:robrob1@ds019078.mlab.com:19078/mernftb',
+  secretOrKey: 'secret',
+};
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,9 @@
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI,
+//   secretOrKey: process.env.SECRET_OR_KEY,
+// };
+
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY,
+  mongoURI: 'mongodb://rob:robrob1@ds019078.mlab.com:19078/mernftb',
+  secretOrKey: 'secret',
 };
