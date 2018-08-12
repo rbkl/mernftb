beb8f176e91a52125c1a3759cb7ed4d44ccab566
Rob <robdawg@gmail.com>
2018-08-12 01:34:24 -0400

    Fixed profile link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -61,7 +61,7 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="profiles"> Developers
+                <Link className="nav-link" to="/profiles"> Developers
                 </Link>
               </li>
             </ul>
